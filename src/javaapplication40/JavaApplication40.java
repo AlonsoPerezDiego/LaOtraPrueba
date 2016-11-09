df21145316333b45467c0b50a762682d36f81bd8
Diego <Diego@192.168.0.16>
2016-11-09 22:21:27 +0100

    JOptionPane + Introduccion y despedida

--- src/javaapplication40/JavaApplication40.java
+++ src/javaapplication40/JavaApplication40.java
@@ -1,6 +1,8 @@
 
 package javaapplication40;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Brais
@@ -8,11 +10,15 @@ package javaapplication40;
 public class JavaApplication40 {
 
     public static void main(String[] args) {
-        int a = 2;
-        int b = 7;
-        int c = 8;
+        int a = 12;
+        int b = 5;
+        int c = 4;
         
-        System.out.println("x= " + (-b+(Math.pow(((Math.pow(b, 2)) -  4*a*c), 1/2)))/(2*a) + "\nX= " + (-b-(Math.pow(((Math.pow(b, 2)) -  4*a*c), 1/2)))/(2*a));
+        JOptionPane.showMessageDialog(null, "Se va a calcular la ecuación: \n" + a + "x^2 + " + b + "x - " + c + " = 0");
+        
+        JOptionPane.showMessageDialog(null, "x= " + (-b+(Math.pow(((Math.pow(b, 2)) -  4*a*-c), 1/2)))/(2*a) + "\nX= " + (-b-(Math.pow(((Math.pow(b, 2)) -  4*a*-c), 1/2)))/(2*a));
+        
+        JOptionPane.showMessageDialog(null, "Gracias por su innecesaria atención. \nLe deseamos unas hermosas blasfemias.");
     }
     
 }
