25e7cb908a372730ac358add14896498dce0f451
Diego <Diego@192.168.0.16>
2016-11-09 22:07:21 +0100

    Reducidas lineas

--- src/javaapplication40/JavaApplication40.java
+++ src/javaapplication40/JavaApplication40.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package javaapplication40;
 
 /**
@@ -11,9 +7,6 @@ package javaapplication40;
  */
 public class JavaApplication40 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         int a = 2;
         int c = 8;
